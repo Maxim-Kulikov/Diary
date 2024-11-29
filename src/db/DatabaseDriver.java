432796548cce21ade24a3cd3357400a0c92c190d
kaimentalityy <kirillsavenko1705@gmail.com>
2024-11-29 19:22:16 +0100

    написал reader и writer для DatabaseDriver

--- src/db/DatabaseDriver.java
+++ src/db/DatabaseDriver.java
@@ -2,23 +2,45 @@ package db;
 
 import server.data.entity.User;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+import java.util.Scanner;
 
 public enum DatabaseDriver {
     INSTANCE;
 
-    public void writeUsersToFile(List<User> userList) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/db/files/users.txt"))) {
-            for (User user : userList) {
-                writer.write(user.toString() + "\n");
+    public void writeUsersToFile(List<User> UserList) {
+        File database = new File("database.txt");
+        try{
+            if(!database.exists()){
+                database.createNewFile();
+            }
+
+            FileWriter fileWriter = new FileWriter(database, true);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(UserList.toString() + "\n");
+            bufferedWriter.close();
+
+        } catch(IOException e) {
+            System.out.println("Error writing to file");
+        }
+    }
+
+    public void readUserFromFile() {
+            try {
+                File myObj = new File("database.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Error");
+                e.printStackTrace();
             }
         }
     }
 
-    public List<User> readUsersFromFile() {
-        return null;
-    }
-}
+
