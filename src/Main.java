3e2a91ba16d8a7cd582e18abd2f6f5acc1a0738e
kaimentalityy <kirillsavenko1705@gmail.com>
2025-01-22 19:44:13 +0100

    TASK-4: added CoonectionPool class

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-import db.DatabaseDriver;
+import db.ConnectionPool;
 
-import server.business.enums.RoleEnum;
-import server.data.entity.User;
-import server.data.repository.UserRepository;
-import server.utils.exception.badrequest.ConstraintViolationException;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws ConstraintViolationException, IOException {
-
-
-        UserRepository userRepository = new UserRepository();
-
-        DatabaseDriver.INSTANCE.writeUsersToFile(List.of(new User(UUID.randomUUID(), "nigger", "123", "kaimentalityy", "Kirill","Savenko", RoleEnum.ADMIN, true)));
-
-
+    public static void main(String[] args) throws SQLException {
     }
 }
